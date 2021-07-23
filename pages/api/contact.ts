180a6bfc48bd7184a16fddb6b12d2664430f828a
alissonph <37026901+alissonph@users.noreply.github.com>
2021-07-23 19:56:28 -0300

    Update contact.ts

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -5,20 +5,24 @@ const EMAIL_API_KEY = process.env.EMAIL_API_KEY || '';
 sgMail.setApiKey(EMAIL_API_KEY);
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { subject, message, name } = req.body
-  const msg = {
-    to: 'rolandoh@outlook.com.br',
-    from: 'contato.barcalocacoes@gmail.com',
-    subject,
-    name,
-    text: message,
-  };
+  if (req.method === 'POST') {
+    const { subject, message, name } = req.body
+    const msg = {
+      to: 'rolandoh@outlook.com.br',
+      from: 'contato.barcalocacoes@gmail.com',
+      subject,
+      name,
+      text: message,
+    };
 
-  try {
-    await sgMail.send(msg);
-    res.json({ message: 'E-mail enviado com sucesso!' });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: 'Erro ao enviar e-mail' });
+    try {
+      await sgMail.send(msg);
+      res.json({ message: 'E-mail enviado com sucesso!' });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error: 'Erro ao enviar e-mail' });
+    }
+  } else {
+    res.status(404).json({ error: 'Método não encontrado.' });
   }
 }
