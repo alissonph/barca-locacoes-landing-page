b44fb7ccb73d359cffe88052dbf9a973d80979cc
Alisson Hausmann <alissonphausmann@hotmail.com>
2022-12-26 23:21:52 -0300

    added analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import "../src/styles/globals.scss";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 import { HomeContextProvider } from "../src/contexts/HomeContext";
 
@@ -7,6 +8,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <HomeContextProvider>
       <Component {...pageProps} />
+      <Analytics />
     </HomeContextProvider>
   );
 }
